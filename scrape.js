2d8d12cee7292c2d794f0126500f7adacf9d02c7
jk01-lab <ateajk01@gmail.com>
2026-04-29 09:28:43 +0300

    Create scrape.js

---
+++ scrape.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+async function getLunch() {
+  const url = "https://ravintolafactory.com/lounasravintolat/ravintolat/factory-virtatalo/";
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+
+  const result = {
+    updated: new Date().toISOString(),
+    week: []
+  };
+
+  $(".list h3").each((i, el) => {
+    const title = $(el).text().trim();
+
+    if (!title.match(/maanantai|tiistai|keskiviikko|torstai|perjantai/i)) return;
+
+    const menuText = $(el).next("p").html();
+
+    const items = menuText
+      .split("<br>")
+      .map(i => i.replace(/<[^>]+>/g, "").trim())
+      .filter(i => i);
+
+    result.week.push({
+      day: title,
+      menu: items
+    });
+  });
+
+  fs.writeFileSync("index.json", JSON.stringify(result, null, 2));
+}
+
+getLunch();
